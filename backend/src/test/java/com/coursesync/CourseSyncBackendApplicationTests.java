dfd01367328ee0f02057c7f1a6a40d3804492875
Nunuman69 <sabitau19@gmail.com>
2025-07-30 19:08:44 -0400

    Intial Setup for Backend using SpringBoot

---
+++ backend/src/test/java/com/coursesync/CourseSyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
